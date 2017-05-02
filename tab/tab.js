f0febdb0b1375f8ad0c5b84f1bf16add1c82b808
ShellyZ <zhangyuyue@coocaa.com>
2017-05-02 18:07:44 +0800

    tab自动切换

--- tab/tab.js
+++ tab/tab.js
@@ -1,4 +1,4 @@
-;(function ($) {
+(function ($) {
     'use strict';
 
     var Tab = function (el, config) {
@@ -8,14 +8,14 @@
             throw 'Tab: `el` must be HTMLElement, and not ' + {}.toString.call(el);
         }
 
-        this.el =  $(el); // root element
+        this.el = $(el); // root element
 
         // 扩展默认配置参数
         this.config = $.extend({
             triggerType: 'click', // 触发事件 click或mouseover
             effect: 'fade', // 切换效果
             active: 1, // 默认选中
-            auto: false // 是否自动切换 false或自动切换时间  
+            auto: false // 是否自动切换 false或自动切换时间
         }, config);
 
         // 保存tab标签列表以及对应的内容列表
@@ -28,7 +28,24 @@
         });
 
         // 当配置了auto为时间, 启用自动切换功能
-        
+        if (config.auto) {
+            // 定义一个全局的定时器
+            this.timer = null;
+            // 计数器
+            this.loop = 0;
+            this.autoPlay();
+
+            this.contentItems.hover(function () {
+                window.clearInterval(_this.timer);
+            }, function () {
+                _this.autoPlay();
+            });
+        }
+
+        // 设置默认显示第几个tab
+        if (config.active > 1) {
+            _this.invoke(_this.navItems.eq(config.active - 1));
+        }
     };
 
     Tab.prototype = {
@@ -47,10 +64,28 @@
             } else if (effect === 'fade') {
                 contentItems.eq(index).fadeIn().siblings().fadeOut();
             }
-            
 
+            // 设置自动切换时 同步下标和loop
+            if (this.config.auto) {
+                this.loop = index;
+            }
+        },
 
+        // 自动播放事件
+        autoPlay: function () {
+            var _this     = this,
+                config    = this.config,
+                navItems  = this.navItems,
+                navLength = navItems.length;
 
+            this.timer = window.setInterval(function () {
+                _this.loop++;
+                if (_this.loop >= navLength) {
+                    _this.loop = 0;
+                }
+                navItems.eq(_this.loop).trigger(config.triggerType);
+
+            }, config.auto);
         }
 
     };
